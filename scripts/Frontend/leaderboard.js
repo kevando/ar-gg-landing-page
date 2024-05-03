09eead9d214361eba7b4a044e2c27426322ae748
kevando <khabich@gmail.com>
2024-05-03 13:40:34 -0700

    demo of high score

--- scripts/Frontend/leaderboard.js
+++ scripts/Frontend/leaderboard.js
@@ -22,27 +22,49 @@ const scoresRef = child(dbRef, `scores/`);
 // On Page Load...
 
 let list = document.querySelector("#LeaderboardList");
+let highScoreText = document.querySelector("#HighScore");
 
-// (function () {
-//   onValue(scoresRef, (snapshot) => {
-//     var scores = [];
+const displayScores = async (snapshot) => {
+  var scores = [];
 
-//     snapshot.forEach((childSnapshot) => {
-//       const childKey = childSnapshot.key;
-//       const childData = childSnapshot.val();
-//       scores.push(childData);
-//     });
+  snapshot.forEach((childSnapshot) => {
+    const childKey = childSnapshot.key;
+    const childData = childSnapshot.val();
+    scores.push(childData);
+  });
 
-//     // sort
+  // sort
 
-//     scores.sort((a, b) => b - a);
+  scores.sort((a, b) => b - a);
 
-//     // display
-//     list.innerHTML = "";
-//     scores.forEach((score, index) => {
-//       const listItem = document.createElement("li");
-//       listItem.innerHTML = `<span>${score}</span>`;
-//       list.appendChild(listItem);
-//     });
-//   });
-// })();
+  // display
+  list.innerHTML = "";
+  scores.forEach((score, index) => {
+    console.log(score);
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `<span>${score}</span>`;
+      list.appendChild(listItem);
+  });
+};
+
+
+const displayHighScore = async (snapshot) => {
+    var scores = [];
+  
+    snapshot.forEach((childSnapshot) => {
+      const childData = childSnapshot.val();
+      scores.push(childData);
+    });
+  
+    // sort
+  
+    scores.sort((a, b) => b - a);
+  
+    // display
+    highScoreText.innerHTML = "HIGH SCORE: " + scores[0];
+    
+  };
+
+(function () {
+  onValue(scoresRef, displayHighScore);
+})();
